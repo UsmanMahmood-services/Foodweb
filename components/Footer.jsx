1a609c83a9decc6b5e8a0215972c692a5d32a0a0
Usman55261350 <UsmanMahmood.services@gmail.com>
2025-12-31 02:07:44 +0500

    Update2

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex justify-center items-center text-center p-2 w-full text-xs  md:text-lg  font-bold bg-gray-700  '>
+    <div className='flex justify-center items-center text-center p-2 w-full text-xs  md:text-lg  font-bold bg-gray-700 text-white  '>
    Copyright &copy; HotBites |  All Rights reserved
     </div>
   )
